d4c07e87120e6368bf2e41b1295adfeaa694d36d
David Cap <david.cap@bocians.net>
2023-04-27 19:26:57 -0600

    add CZ translation for translation module

---
+++ Resources/lang/translation/cs/translations.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'title' => [
+        'translations' => 'Překlad',
+        'edit translation' => 'Upravit překlad',
+    ],
+    'button' => [
+        'create translation' => 'Vytvořit překlad',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+    'Clear translation cache' => 'Vyčistit cache překladů',
+    'Export' => 'Export',
+    'Import' => 'Import',
+    'Import csv translations file' => 'Importovat CSV soubor s překlady',
+    'only for developers' => 'Will require git management!',
+    'Translations exported' => 'Překlady exportovány',
+    'Translations imported' => 'Překlady importovány',
+    'select CSV file' => 'Vyberte CSV soubor ...',
+    'csv only allowed' => 'Prosím vyberte pouze CSV souboryPlease upload a CSV file only.',
+    'time' => 'Čas',
+    'author' => 'Autor',
+    'history' => 'Historie',
+    'No Revisions yet' => 'Zatím žádné revize',
+    'event' => 'Událost',
+    'created' => 'Vytvořeno',
+    'edited' => 'Upraveno',
+    'revert history' => 'Vrátit historii',
+    'list resource' => 'Seznam překladů',
+    'edit resource' => 'Upravit překlad',
+    'import resource' => 'Importovat překlady',
+    'export resource' => 'Exportovat překlady',
+];
