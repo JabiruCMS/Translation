5942cf4bce436a334b5b1ac576b475a1b0b27958
David Cap <david.cap@bocians.net>
2023-05-07 15:15:09 -0600

    rename core translations

---
+++ Resources/lang/_core/cs/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'site-name' => 'Název webu',
+    'site-name-mini' => 'Název webu (krátký)',
+    'site-description' => 'Popis webu',
+    'template' => 'Frontend šablona',
+    'analytics-script' => 'Analytics skript',
+    'locales' => 'Lokalizace',
+];
